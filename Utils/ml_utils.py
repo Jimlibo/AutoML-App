ac46888c52f6e4ce650667f254f2ff415874c58d
lymperop <jimlibo13@gmail.com>
2023-06-21 16:49:12 +0300

    added initial content function for the general tab

--- Utils/ml_utils.py
+++ Utils/ml_utils.py
@@ -30,3 +30,31 @@ def create_model(task, train_df, target, model_name_):
         regression.save_model(best_model_, os.path.join("Models", model_name_))
 
     return best_model_
+
+
+# Further enhance overview function - TODO
+def overview():
+    st.title("Welcome to AutoML App!")
+    st.write("This app has 6 tabs, including General, Import Dataset, EDA, Create Model,"
+             " Download Model, and Deploy Model.")
+
+    # Add descriptions for each tab
+    st.header("General")
+    st.write("This is the overview page you are currently viewing. It provides an overview of the entire app.")
+
+    st.header("Import Dataset")
+    st.write("In this tab, you can import your dataset to be used for analysis and model building.")
+
+    st.header("EDA")
+    st.write("This tab allows you to explore your dataset using various visualizations and statistical methods.")
+
+    st.header("Create Model")
+    st.write("In this tab, you can build and train a machine learning model on your dataset.")
+
+    st.header("Download Model")
+    st.write("This tab allows you to download your trained machine learning model for future use.")
+
+    st.header("Deploy Model")
+    st.write("In this tab, you can deploy your trained machine learning model to a cloud service or a local environment"
+             " for inference.")
+
