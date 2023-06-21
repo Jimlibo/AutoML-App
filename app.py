efa17d8d5c011c9bac2cbfcf8d517e92cda7bec2
lymperop <jimlibo13@gmail.com>
2023-06-21 15:02:52 +0300

    moved create_model() to ml_utils.py

--- app.py
+++ app.py
@@ -8,30 +8,8 @@ import os
 import pandas as pd
 import streamlit as st
 import pandas_profiling
-from pycaret import regression, classification
 from streamlit_pandas_profiling import st_profile_report
-
-
-def create_model(task, train_df, target, model_name_):
-    best_model_ = None
-
-    if task == 'Classification':
-        classification.setup(train_df, target=target, verbose=False)
-        setup_df = classification.pull()
-        st.dataframe(setup_df)
-        best_model_ = classification.compare_models()
-        compare_df = classification.pull()
-        st.dataframe(compare_df)
-        classification.save_model(best_model_, os.path.join("Models", model_name_))
-
-    if task == 'Regression':
-        regression.setup(train_df, target=target, verbose=False)
-        st.dataframe(regression.pull())
-        best_model_ = regression.compare_models()
-        st.dataframe(regression.pull())
-        regression.save_model(best_model_, os.path.join("Models", model_name_))
-
-    return best_model_
+from Utils.ml_utils import create_model
 
 
 # Initialization
