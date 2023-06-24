5a666568b1c56092879b09b23eb0719103b0e659
lymperop <jimlibo13@gmail.com>
2023-06-24 13:14:51 +0300

    created script to automate the app's setup

---
+++ start-automl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# define color codes for output formatting
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Go to Setup directory
+cd Setup
+
+# Check if docker container exists - if not create it
+if [ ! "$(docker ps -a | grep automl)" ]; then
+    # create docker image named automl
+    echo -e "${GREEN}Creating automl image...${NC}";
+    docker build -t automl .
+
+    # create docker container named automl
+    echo -e "${GREEN}Creating and starting automl container...${NC}";
+    docker run --name automl -p 8501:8501 automl;
+
+else
+    # else, simply start the container
+    echo -e "${GREEN}Starting automl container...${NC}";
+    docker start automl;
+fi
