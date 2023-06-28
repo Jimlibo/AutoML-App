0eb1ab0c6a6385ee7faf44a63d7cc942729ad445
lymperop <jimlibo13@gmail.com>
2023-06-28 17:58:03 +0300

    added support for non-dockerized app

--- start-automl.sh
+++ start-automl.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
+# function to check if a specific service exists in the os
+service_exists() {
+    local n=$1
+    if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | sed 's/^\s*//g' | cut -f1 -d' ') == $n.service ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
 # define color codes for output formatting
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
@@ -7,18 +19,29 @@ NC='\033[0m' # No Color
 # Go to Setup directory
 cd Setup
 
-# Check if docker container exists - if not create it
-if [ ! "$(docker ps -a | grep automl)" ]; then
-    # create docker image named automl
-    echo -e "${GREEN}Creating automl image...${NC}";
-    docker build -t automl:latest .
+# check if docker exists and if yes, use it to setup and start the app
+if service_exists docker; then
 
-    # create docker container named automl
-    echo -e "${GREEN}Creating and starting automl container...${NC}";
-    docker run  --name automl -p 8501:8501 automl:latest;
+    # Check if docker container exists - if not create it
+    if [ ! "$(docker ps -a | grep automl)" ]; then
+        # create docker image named automl
+        echo -e "${GREEN}Creating automl image...${NC}";
+        docker build -t automl:latest .
+
+        # create docker container named automl
+        echo -e "${GREEN}Creating and starting automl container...${NC}";
+        docker run  --name automl -p 8501:8501 automl:latest;
+
+    else
+        # else, simply start the container
+        echo -e "${GREEN}Starting automl container...${NC}";
+        docker start automl;
+    fi
 
 else
-    # else, simply start the container
-    echo -e "${GREEN}Starting automl container...${NC}";
-    docker start automl;
+    # if docker does not exist, use python and streamlit to run the app
+    pip install -r requirements.txt   # download necessary packages directly into python
+    cd ../ 
+    streamlit run app.py   # run the app
+
 fi
--- stop-automl.sh
+++ stop-automl.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+
+# function to check if a specific service exists in the os
+service_exists() {
+    local n=$1
+    if [[ $(systemctl list-units --all -t service --full --no-legend "$n.service" | sed 's/^\s*//g' | cut -f1 -d' ') == $n.service ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
 # code for optional bash parameter --remove-container
 REMOVE_CONTAINERS=false
 while [ "$1" ]; do
@@ -15,14 +27,25 @@ while [ "$1" ]; do
 GREEN='\033[0;32m'  # green color
 NC='\033[0m' # No Color
 
-# stop app by shutting down the container
-echo -e "${GREEN}Shutting down AutoML...${NC}"
-docker stop automl
+# check if docker exists, and if yes, use it to stop the app
+if service_exists docker; then
+    # stop app by shutting down the container
+    echo -e "${GREEN}Shutting down AutoML...${NC}"
+    docker stop automl
 
-if "$REMOVE_CONTAINERS"; then
-    echo -e "${GREEN}Removing docker container and images...${NC}"
+    if "$REMOVE_CONTAINERS"; then
+        echo -e "${GREEN}Removing docker container and images...${NC}"
 
-    docker rm automl      # remove kafka container
-    docker image rm automl:latest # remove automl image
-    docker volume prune -f   # remove unused volumes
+        docker rm automl      # remove kafka container
+        docker image rm automl:latest # remove automl image
+        docker volume prune -f   # remove unused volumes
+    fi
+
+else
+    # if docker does not exist, kill the streamlit process
+    echo -e "${GREEN}Shutting down AutoML...${NC}"
+    kill `pgrep streamlit`
+    
 fi
+
+echo -e "${GREEN}AutoML app was successfully shutted down!${NC}"
